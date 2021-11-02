d3e642e83579658588935563f944337a8402f2af
ZaidMarrie <zaidmarrie27@gmail.com>
2021-11-02 15:46:47 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// SCSS Task
+
+// JS Task
+
+// BrowserSync Tasks
+
+// Watch Task 
+
+// Default Gulp Task
