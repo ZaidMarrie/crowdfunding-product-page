08e28f4b23bc7d01555937fa7c146a8af7775392
ZaidMarrie <zaidmarrie27@gmail.com>
2021-11-07 14:13:21 +0200

    setup gulp task and sass boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,48 @@ const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // SCSS Task
+function scssTask() {
+    return src('src/scss/style.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('dist', {sourcemaps: '.'}));
+}
 
 // JS Task
-
+function jsTask() {
+    return src('src/js/script.js', { sourcemaps: true })
+        .pipe(babel({ presets: ['@babel/preset-env'] }))
+        .pipe(terser())
+        .pipe(dest('dist', { sourcemaps: '.' }));
+}
 // BrowserSync Tasks
+function browserSyncServe(cb) {
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb();
+}
+function browserSyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
 
 // Watch Task 
+function watchTask() {
+    watch('**/*.html', browserSyncReload);
+    watch(
+        ['src/scss/**/*.scss', 'src/js/**/*.js'],
+        series(scssTask, jsTask, browserSyncReload)
+    );
+}
 
 // Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
