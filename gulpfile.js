33f9c2a86f3b8c301f8927fcd6089be155ccf519
Zaid Marrie <zaidmarrie27@gmail.com>
2022-09-17 14:25:49 +0200

    Add scripts for development and production

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,61 @@
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const babel = require('gulp-babel');
-const terser = require('gulp-terser');
-const browsersync = require('browser-sync').create();
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("autoprefixer");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const babel = require("gulp-babel");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
 
 // SCSS Task
 function scssTask() {
-    return src('src/scss/style.scss', { sourcemaps: true })
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(dest('dist', {sourcemaps: '.'}));
+	return src("src/scss/style.scss", { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest("dist", { sourcemaps: "." }));
 }
 
 // JS Task
 function jsTask() {
-    return src('src/js/script.js', { sourcemaps: true })
-        .pipe(babel({ presets: ['@babel/preset-env'] }))
-        .pipe(terser())
-        .pipe(dest('dist', { sourcemaps: '.' }));
+	return src("src/js/script.js", { sourcemaps: true })
+		.pipe(babel({ presets: ["@babel/preset-env"] }))
+		.pipe(terser())
+		.pipe(dest("dist", { sourcemaps: "." }));
 }
+
+// Copy Assets
+function copyAssets() {
+	return src("src/assets/**/*.{png, jpg, svg}").pipe(dest("dist"));
+}
+
 // BrowserSync Tasks
 function browserSyncServe(cb) {
-    browsersync.init({
-        server: {
-            baseDir: '.',
-        },
-        notify: {
-            styles: {
-                top: 'auto',
-                bottom: '0',
-            },
-        },
-    });
-    cb();
+	browsersync.init({
+		server: {
+			baseDir: ".",
+		},
+		notify: {
+			styles: {
+				top: "auto",
+				bottom: "0",
+			},
+		},
+	});
+	cb();
 }
 function browserSyncReload(cb) {
-    browsersync.reload();
-    cb();
+	browsersync.reload();
+	cb();
 }
 
-// Watch Task 
+// Watch Task
 function watchTask() {
-    watch('**/*.html', browserSyncReload);
-    watch(
-        ['src/scss/**/*.scss', 'src/js/**/*.js'],
-        series(scssTask, jsTask, browserSyncReload)
-    );
+	watch("**/*.html", browserSyncReload);
+	watch(["src/scss/**/*.scss", "src/js/**/*.js"], series(scssTask, jsTask, browserSyncReload));
 }
+
+// Build Task
+exports.build = series(scssTask, jsTask, copyAssets);
 
 // Default Gulp Task
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+exports.default = series(scssTask, jsTask, copyAssets, browserSyncServe, watchTask);
